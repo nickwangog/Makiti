b4d1e96cdb310134e5d4b582102568bbf19697c6
Nicholas Wang <nwang@e1z3r4p24.42.us.org>
2018-08-20 15:51:07 -0700

    ningi

--- Appstore/Back_End/zip_checker.py
+++ Appstore/Back_End/zip_checker.py
@@ -8,16 +8,17 @@ path ='/nfs/2017/n/nwang/projects/Ford/Appstore/Back_End/Zip_Folder'
 list_of_zips = open('test.txt', 'r+w')
 contents = list_of_zips.readlines()
 contents = [x.strip() for x in contents]
+zip_contents = open('zip_contents.txt', 'r+w')
 
 for filename in glob.glob(os.path.join(path, '*.gz')):
     if filename not in contents:
         list_of_zips.write("%s\n" % filename)
-        # f = gzip.open(filename, 'rb')
-        # file_content = f.read()
-        # print file_content
-        # f.close()
-        # if authenticate.checkFile(filename) == 1:
-        #     print ("installed " + filename)
+        f = gzip.open(filename, 'r')
+        file_content = f.read()
+        zip_contents.write("%s\n" % file_content)
+        f.close()
+        if authenticate.checkFile("zip_contents.txt") == 1:
+             print ("installed " + filename)
             #install it 
    
 # print(contents)
