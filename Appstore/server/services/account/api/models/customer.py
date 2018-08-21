a3495a0d57e50ea04ce0ba25f9362e1e8d373b80
dmontoyain <dmontoyainestroza@gmail.com>
2018-08-20 18:46:06 -0700

    customer

--- Appstore/server/services/account/api/models/customer.py
+++ Appstore/server/services/account/api/models/customer.py
@@ -7,6 +7,7 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(50), nullable=False)
     lastname = db.Column(db.String(50), nullable=False)
+    password = db.Column()
     carid = db.Column(db.Integer, nullable=False)
 
     def __init__(self, firstname, lastname, carid):
