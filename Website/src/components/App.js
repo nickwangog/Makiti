adfaefd7650a37040fc0384258b36130a6f27c21
Nicholas Wang <nwang@e1z3r4p24.42.us.org>
2018-08-26 01:51:16 -0700

    admin modals

--- Website/src/components/App.js
+++ Website/src/components/App.js
@@ -144,7 +144,7 @@ class App extends React.Component {
 						<Redirect from="/home" to="/" />
 						<PrivateRoute path="/client" accessCheck={isCus} render={() => (<Client appState={this.state} />)} />
 						<PrivateRoute path="/developer" accessCheck={true} render={() => (<Developer appState={this.state} />)} />
-						<PrivateRoute path="/admin" accessCheck={isAd} render={() => (<Admin appState={this.state} />)}  />
+						<PrivateRoute path="/admin" accessCheck={true} render={() => (<Admin appState={this.state} />)}  />
 						<PrivateRoute path="/accountsettings" accessCheck={isCus} render={() => (<AccountSettings appState={this.state} />)} />
 						<Route component={NotFound} />
 					</Switch>
